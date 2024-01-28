534cc3bf9af00e5df26155db8159f0d60f2ab06c
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-01-28 13:45:05 -0600

    Create 668. Kth Smallest Number in Multiplication Table.cpp

---
+++ LeetCode/Hard/Binary Search/668. Kth Smallest Number in Multiplication Table.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findKthNumber(int m, int n, int k) {
+        int low = 1;
+        int high = m*n;
+        while(low<high){
+            int mid = low + (high-low)/2;
+            if(k>x(mid, n, m)){
+                low = mid+1;
+            }else{
+                high = mid;
+            }
+        }
+        return low;
+    }
+private:
+    int x(int t, int n, int m){
+        int z = 0;
+        for(int i = 1; i<=n; i++){
+            z+=min(t/i, m);
+        }
+        return z;
+    }
+};
