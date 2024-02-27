4bc0b75f0f7ab23fd3937e891a8c580b321b0a27
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-02-26 20:27:12 -0600

    Update and rename 543. Diameter of a Binary Tree to 543. Diameter of a Binary Tree .cpp

--- LeetCode/Easy/DFS and BFS/543. Diameter of a Binary Tree .cpp
+++ LeetCode/Easy/DFS and BFS/543. Diameter of a Binary Tree .cpp
@@ -10,13 +10,15 @@ private:
     int calculateDiameter(TreeNode* root, int& maxDiameter) {
         if (!root)
             return 0;
-
+        //basically calcualte the depth of a current node aka untill it reaches a leaf node, then at the current ndoe as the base
+        //add the left and right 
         int leftDepth = calculateDiameter(root->left, maxDiameter);
         int rightDepth = calculateDiameter(root->right, maxDiameter);
 
         maxDiameter = max(maxDiameter, leftDepth + rightDepth);
 
         // Return the depth of the current node
+        //max between goign all the way down is the max depth of any node
         return 1 + max(leftDepth, rightDepth);
     }
 };
