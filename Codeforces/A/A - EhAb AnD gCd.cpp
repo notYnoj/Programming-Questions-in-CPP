b7e0a9803f9bc89b9a9a4cd52580ed33320b4e55
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-03-20 21:46:05 -0500

    Create A - EhAb AnD gCd.cpp

---
+++ Codeforces/A/A - EhAb AnD gCd.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        cout<<1<<" "<<x-1<<"\n";
+    }
+ 
+}
+//this always works.
