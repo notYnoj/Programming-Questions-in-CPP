69595a79cb8c684219af63af8b8a37f4d4b38bb9
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-06-07 14:37:08 -0500

    Create A - Bear and Friendship Condition.cpp

---
+++ Codeforces/A/A - Bear and Friendship Condition.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll n, m;
+const int mxN = 150010;
+vector<ll> grid[mxN];
+ll parent[mxN], c[mxN];
+ll gauss(ll a){
+    return (a*(a-1)/2);
+}
+ll get(ll a){
+    if(parent[a] == a){
+        return a;
+    }else{
+        return parent[a] = get(parent[a]);
+    }
+}
+void merge(ll a, ll b){
+    int x= get(a);
+    int y = get(b);
+    if(x == y){
+        return;
+    }else{
+        c[x] += c[y];
+        parent[y] = x;
+    }
+}
+int main(){
+    cin>>n>>m;
+    for(int i = 1; i<=n; i++){
+        parent[i]=i;
+        c[i] = 1;
+    }
+    for(int i = 0; i<m; i++){
+        ll a,b;
+        cin>>a>>b;
+        grid[a].push_back(b);
+        grid[b].push_back(a);
+        merge(a,b);
+    }
+    bool check = true;
+    map<ll, ll> mp;
+    for(ll i = 1; i<=n; i++){
+        mp[get(i)] += grid[i].size();
+    }
+
+    for(ll i = 1; i<=n; i++){
+        ll a = get(i);
+        ll g = gauss(c[a]);
+        if((mp[a]/2) != g){
+            check = false;
+            break;
+        }
+    }
+    if(check){
+        cout<<"YES";
+    }else{
+        cout<<"NO";
+    }
+}
