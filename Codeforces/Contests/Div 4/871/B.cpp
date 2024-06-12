d5c0c9133feaef1886faf695f6c5303005c88a93
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-06-12 13:29:47 -0500

    Update B.cpp

--- Codeforces/Contests/Div 4/871/B.cpp
+++ Codeforces/Contests/Div 4/871/B.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int i;
+        cin>>i;
+        int ans = 0;
+        int length = 0;
+        for(int j = 0;j<i; j++){
+            int t;
+            cin>>t;
+            if(t == 0) length++;
+            else length = 0;
+            ans = max(length, ans);
+
+        }
+        cout<<ans<<"\n";
+
+    }
+}
