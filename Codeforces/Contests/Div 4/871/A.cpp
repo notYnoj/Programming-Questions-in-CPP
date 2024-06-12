0e4db1de3c5dd97db2d2a9fb1e57b8679d7c9674
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-06-12 13:29:19 -0500

    Update A.cpp

--- Codeforces/Contests/Div 4/871/A.cpp
+++ Codeforces/Contests/Div 4/871/A.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--) {
+        string s = "codeforces";
+        string s1;
+        cin >> s1;
+        int ans = 0;
+        for(int i = 0; i<10; i++){
+            if(s1[i] != s[i]) ans++;
+        }
+        cout<<ans<<'\n';
+    }
+
+}
