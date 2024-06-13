5c39e08413a2b0a3c2f09ee5ac7cd976510ad250
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-06-13 01:15:14 -0500

    Update F.cpp

--- Codeforces/Contests/Div 3/479/F.cpp
+++ Codeforces/Contests/Div 3/479/F.cpp
@@ -1,41 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int mxn = 1e9+5;
-long long dp[mxn];
-int main(){
-    memset(dp, 0, sizeof(dp));
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int n;
-    cin>>n;
+    cin >> n;
     vector<int> arr(n);
-    for(int i = 0; i<n; i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-    int mn = INT_MAX;
-    int mx = 0;
-    map<int, vector<int>> mp;
-    for(int i = 0; i<n; i++){
+
+    map<int, int> mp;
+    vector<int> dp(n, 0);
+    dp[0] = 1;
+    mp[arr[0]] = 1;
+    int s = arr[0];
+    for (int i = 1; i < n; i++) {
         int a = arr[i];
-        dp[a] = dp[a-1]+1;
-        if(dp[a]>1) {
-            vector<int> aaa = mp[a - 1];
-            aaa.push_back(a);
-            mp[a] = aaa;
+        mp[a] = mp[a-1]+1;
+        if(dp[i-1]<mp[a]){
+            s =a;
+            dp[i] = mp[a];
         }else{
-            mp[a] = {a};
-        }
-        mn = min(a, mn);
-        mx = max(a, mx);
-    }
-    int t = 0;
-    int nod = 0;
-    for(int i = mn; i<=mx; i++){
-        if(dp[i]>t){
-            t = dp[i];
-            nod = i;
+            dp[i] = dp[i-1];
         }
     }
-    cout<<t<<"\n";
-    for(int i: mp[nod]){
-        cout<<i<<" ";
+    cout << dp[n - 1] << "\n";
+    vector<int> at;
+    for(int i = 0; i<n; i++){
+        if(arr[i] == s-dp[n-1]+1){
+            at.push_back(i+1);
+            s++;
+        }
+    }
+    for (int i: at) {
+        cout << i << " ";
     }
 }
