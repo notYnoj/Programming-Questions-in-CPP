b2d9f6e8bddb94d399344cf31b5fc052adeddfce
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-06-22 21:21:23 -0500

    Create PalindromeGame.cpp

---
+++ USACO/Bronze/PalindromeGame.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        cout<<((!n%10)?"E":"B");
+    }
+}
