de2a8fb71769f49ca3881951c85722759ef4c1e3
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-06-25 19:50:13 -0500

    The correct solution!
    haha i forgot to like read the question :D
    but it wasnt hard when ykyk u read :D

--- Codeforces/D/D - Buying Gifts.cpp
+++ Codeforces/D/D - Buying Gifts.cpp
@@ -2,42 +2,50 @@
 using namespace std;
 
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
     int t;
     cin>>t;
     while(t--){
         int n;
         cin>>n;
-        vector<int> first(n);
-        vector<int> second(n);
+        vector<pair<int, int>> stores(n);
         for(int i = 0; i<n; i++){
-            cin>>first[i];
+            int a, b;
+            cin>>a>>b;
+            stores[i] = {a,b};
         }
+        multiset<int> usable;
+        multiset<int> bigger;
         for(int i = 0; i<n; i++){
-            cin>>second[i];
+            bigger.insert(stores[i].second);
         }
-        vector<int> copy = second;
-        sort(copy.begin(), copy.end());
+        sort(stores.begin(), stores.end());
         int ans = INT_MAX;
-        for(int i = 1; i<n; i++){
-            ans = min(ans, abs(first[0]-second[i]));
-        }
-        if(ans == 0){
-            cout<<0<<'\n';
-        }else{
-            //bs
-            for(int i = 1; i<n; i++){
-                int cur = first[i];
-                auto it = lower_bound(copy.begin(), copy.end(), cur);
-                auto it2 = copy.end();
-                if(it != copy.begin()) it2 = prev(it);
-                if(it2 == copy.end()){
-                    if(*it != second[i]) ans = min(ans, abs(cur - *it));
-                    else if() ans = min(ans, abs(cur - *next(it)));
-                }else{
-
-                }
+        for(int i = 0; i<n-1; i++){
+            auto cur = stores[i];
+            bigger.erase(bigger.find(cur.second));
+            auto possible = usable.lower_bound(cur.first);
+            auto possible2 = usable.end();
+            if(possible != usable.begin()) possible2 = prev(possible);
+            if(possible2 != usable.end()){
+                ans = min(ans, abs(cur.first - *(--bigger.end())));
+                if(*(--bigger.end())<(possible!=usable.end()?*possible:INT_MIN)) ans = min(ans, abs(cur.first - *possible));
+                if(*(--bigger.end())<(possible2!=usable.end()?*possible2:INT_MIN))ans = min(ans, abs(cur.first - *possible2));
+            }else {
+                int best = abs(cur.first - *possible);
+                if (*(--bigger.end()) > *possible) ans = min(ans, abs(cur.first - *(--bigger.end())));
+                else ans = min(ans, min(best, abs(cur.first - *(--bigger.end()))));
             }
-            cout << ans << '\n';
+            usable.insert(cur.second);
         }
+        //last one
+        auto nwords = usable.lower_bound(stores[n-1].first);
+        auto nwords2 = (nwords!=usable.begin()?prev(nwords): usable.end());
+        int t1 = (nwords != usable.end()? *nwords: -1000000001);
+        int t2 = (nwords2 != usable.end()? *nwords2: -1000000001);
+        int bestest = min(abs(stores[n-1].first - t1),abs(stores[n-1].first-t2));
+        ans = min(ans, bestest);
+        cout<<ans<<'\n';
     }
 }
