58d1f4b7649a07b9ccca9ae7ca8061e9ff0ffa7b
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-06-27 23:38:19 -0500

    Update D - Beauty of the mountains.cpp

--- Codeforces/D/D - Beauty of the mountains.cpp
+++ Codeforces/D/D - Beauty of the mountains.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t;
-    cin>>t;
+    int t = 1;
+    //cin>>t;
     while(t--){
         int n,m,k;
         cin>>n>>m>>k;
         vector<vector<int>> grid(n, vector<int>(m));
         vector<vector<int>> needed(n, vector<int>(m));
         int mon1 = 0, mon2 = 0;
-        for(int i = 0 ;i<n; i++){
+        for(int i = 0;i<n; i++){
             for(int j = 0; j<m; j++){
                 cin>>grid[i][j];
             }
@@ -20,9 +20,34 @@ int main(){
         for(int i = 0 ;i<n; i++){
             for(int j = 0; j<m; j++){
                 cin>>needed[i][j];
-                if(needed[i][j] == 1?mon1+=grid[i][j]: mon2+=grid[i][j]);
+                needed[i][j] == 1?mon1+=grid[i][j]: mon2+=grid[i][j];
             }
         }
-        
+        if(k == 1 || mon1 == mon2) cout<<"YES\n";
+        else{
+            //kxk -check, check will be for wihtout them, with them will be kxk-check
+            set<int> check;
+            vector<vector<int>> pref(n, vector<int>(m, 0));
+            for(int i = 0; i<n; i++){
+                for(int j = 0; j<m; j++){
+                    pref[i][j] = needed[i][j];
+                    if(i>0){
+                        pref[i][j]+=pref[i-1][j];
+                    }
+                    if(j>0){
+                        pref[i][j]+=pref[i][j-1];
+                    }
+                    if(i>0 && j>0){
+                        pref[i][j] -= pref[i-1][j-1];
+                    }
+                }
+            }
+            for(int i = 0; i<n; i++){
+                for(int j = 0; j<m; j++){
+                    cout<<pref[i][j]<<' ';
+                }
+                cout<<'\n';
+            }
+        }
     }
 }
