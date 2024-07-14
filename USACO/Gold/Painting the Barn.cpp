5cd402d0598825f092adc2a389728a6690a63616
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-07-13 21:41:39 -0500

    Update Painting the Barn.cpp

--- USACO/Gold/Painting the Barn.cpp
+++ USACO/Gold/Painting the Barn.cpp
@@ -35,12 +35,64 @@ int main(){
             a[i][j] += (a[i-1][j] + a[i][j-1] - a[i-1][j-1]);
         }
     }
+    for(int i = 0; i<WIDTH; i++){
+        for(int j = 0; j<WIDTH; j++){
+            cout<<a[i][j]<<' ';
+        }
+        cout<<'\n';
+    }
+
     auto getRec= [=](int x1, int y1, int x2, int y2)->int{
         assert(x2>=x1 && y2>=y1);
         int ans = a[x2][y2] - (x1>0?a[x1-1][y2]:0) - (y1>0?a[x2][y1-1]:0) + (y1>0 && x1>0?a[x1-1][y1-1]:0);
         return ans;
     };
-    vector<int> up(WIDTH), down(WIDTH), left(WIDTH), right(WIDTH);
-    
+    vector<int> up(WIDTH+1, 0), down(WIDTH+1, 0), left(WIDTH+1, 0), right(WIDTH+1, 0);
+    //2d kadane algo, with dividng by i where is row i(top-bottom) or col i (left-right) since row i can init of itself provide contribution
+    //we wnat to make sure that 1 takes any contrbution there, thus have only top start on i and bottom be strictely bigger, same for right(on I)/left(less than)
+
+    for(int top = 0; top<WIDTH; top++){
+        for(int bottom = top; bottom<WIDTH; bottom++){
+            int lf = 0;
+            for(int rows = 0; rows<WIDTH; rows++){
+                int cur_sum = getRec(top, lf, bottom, rows);
+                int this_row = getRec(top, rows, bottom, rows);
+                if(cur_sum<this_row){
+                    lf = rows;
+                    if(top != 0) down[top-1] = max(down[top-1], this_row);
+                    up[bottom] = max(up[bottom], this_row);
+                    if(rows+1 != WIDTH){
+                        left[rows+1] = max(left[rows+1], this_row);
+                    }
+                    right[lf] = max(right[lf], this_row);
+                }else{
+                    if(top != 0) down[top-1] = max(down[top-1], cur_sum);
+                    up[bottom] = max(up[bottom], cur_sum);
+                    if(rows+1 != WIDTH){
+                        left[rows+1] = max(left[rows+1], cur_sum);
+                    }
+                    right[lf] = max(right[lf], cur_sum);
+                }
+            }
+        }
+    }
+    for (int i = 1; i < WIDTH; i++) {
+        up[i] = max(up[i], up[i - 1]);
+        left[i] = max(left[i], left[i - 1]);
+    }
+    for (int i = WIDTH - 2; i >= 0; i--) {
+        down[i] = max(down[i], down[i + 1]);
+        right[i] = max(right[i], right[i + 1]);
+    }
+    int ans = 0;
+    for(int i =0; i<WIDTH; i++){
+
+        ans = max(ans, up[i]+down[i]);
+    }
+    for(int i = 0; i<WIDTH; i++){
+
+        ans = max(ans, left[i]+right[i]);
+    }
+    cout<<ans+ans1;
 
 }
