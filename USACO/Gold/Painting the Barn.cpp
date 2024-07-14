22054ab934d5353fc9e487ea537258f1b271c5e6
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-07-13 22:24:23 -0500

    Update Painting the Barn.cpp

--- USACO/Gold/Painting the Barn.cpp
+++ USACO/Gold/Painting the Barn.cpp
@@ -1,62 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int WIDTH = 201;
-int main(){
-    vector<vector<int>> pref(WIDTH, vector<int>(WIDTH));
-    int n,k;
-    cin>>n>>k;
-    for(int i =0; i<n; i++){
-        int x1,y1,x2,y2;
-        cin>>x1>>y1>>x2>>y2;
-        for(int h = x1; h<=x2; h++){
-            pref[h][y1]++;
-            if(y2+1 <=200){
-                pref[h][y2+1]--;
+void assert_with_error_code(bool condition, int i) {
+    if (!condition) {
+        std::cerr << "Line, failed at " <<i<< std::endl;
+        std::exit(1);
+    }
+}
+int main() {
+    //std::ifstream read("paintbarn.in");
+
+    int rect_num;
+    int optimal_amt;
+    cin >> rect_num >> optimal_amt;
+
+    vector<vector<int>> barn(WIDTH, vector<int>(WIDTH));
+    for (int r = 0; r < rect_num; r++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        for (int y = y1; y < y2; y++) {
+            barn[y][x1]++;
+            if (x2 < WIDTH) { barn[y][x2]--; }
+        }
+    }
+
+    for (int r = 0; r < WIDTH; r++) {
+        int so_far = 0;
+        for (int c = 0; c < WIDTH; c++) {
+            so_far += barn[r][c];
+            barn[r][c] = so_far;
+        }
+    }
+
+    /*
+     * leftovers[r][c] = if we paint the cell there,
+     * gives the amount of change in optimal paint size
+     */
+    vector<vector<int>> leftovers(WIDTH, vector<int>(WIDTH));
+    int rn_amt = 0;
+    for (int r = 0; r < WIDTH; r++) {
+        for (int c = 0; c < WIDTH; c++) {
+            if (barn[r][c] == optimal_amt) {
+                leftovers[r][c] = -1;
+                rn_amt++;
+            } else if (barn[r][c] == optimal_amt - 1) {
+                leftovers[r][c] = 1;
             }
         }
     }
-    int ans1 = 0;
-    for(int i =0; i<WIDTH; i++){
-        int cur = 0;
-        for(int j = 0; j<WIDTH; j++){
-            cur+=pref[i][j];
-            pref[i][j] = cur;
-            if(pref[i][j] == k) ans1++;
-        }
-    }
-    vector<vector<int>> a(WIDTH, vector<int>(WIDTH));
-    for(int i = 0; i<WIDTH; i++){
-        a[i][0] = (pref[i][0]+1 == k?1:(pref[i][0] == k?-1:0));
-        a[0][i] = (pref[0][i]+1 == k?1:(pref[i][0] == k?-1:0));
-    }
-    for(int i =1; i<WIDTH; i++){
-        for(int j = 1; j<WIDTH; j++){
-            a[i][j] = (pref[i][j]+1 == k?1:(pref[i][j] == k?-1:0));
-            a[i][j] += (a[i-1][j] + a[i][j-1] - a[i-1][j-1]);
-        }
-    }
-    for(int i = 0; i<WIDTH; i++){
-        for(int j = 0; j<WIDTH; j++){
-            cout<<a[i][j]<<' ';
-        }
-        cout<<'\n';
-    }
 
-    auto getRec= [=](int x1, int y1, int x2, int y2)->int{
-        assert(x2>=x1 && y2>=y1);
-        int ans = a[x2][y2] - (x1>0?a[x1-1][y2]:0) - (y1>0?a[x2][y1-1]:0) + (y1>0 && x1>0?a[x1-1][y1-1]:0);
-        return ans;
+    // create a prefix sum array for easy 2d querying the leftovers array
+    vector<vector<int>> pref_leftovers(WIDTH + 1, vector<int>(WIDTH + 1));
+    for (int r = 1; r < WIDTH + 1; r++) {
+        for (int c = 1; c < WIDTH + 1; c++) {
+            pref_leftovers[r][c] =
+                    (pref_leftovers[r - 1][c] + pref_leftovers[r][c - 1] -
+                     pref_leftovers[r - 1][c - 1] + leftovers[r - 1][c - 1]);
+        }
+    }
+    // returns the sum of leftovers[from_r][from_c] to leftovers[to_r][to_c]
+    auto getRec = [&](int from_r, int from_c, int to_r, int to_c) {
+        return (pref_leftovers[to_r + 1][to_c + 1] -
+                pref_leftovers[from_r][to_c + 1] -
+                pref_leftovers[to_r + 1][from_c] +
+                pref_leftovers[from_r][from_c]);
     };
     vector<int> up(WIDTH+1, 0), down(WIDTH+1, 0), left(WIDTH+1, 0), right(WIDTH+1, 0);
     //2d kadane algo, with dividng by i where is row i(top-bottom) or col i (left-right) since row i can init of itself provide contribution
     //we wnat to make sure that 1 takes any contrbution there, thus have only top start on i and bottom be strictely bigger, same for right(on I)/left(less than)
-
     for(int top = 0; top<WIDTH; top++){
         for(int bottom = top; bottom<WIDTH; bottom++){
             int lf = 0;
             for(int rows = 0; rows<WIDTH; rows++){
-                int cur_sum = getRec(top, lf, bottom, rows);
-                int this_row = getRec(top, rows, bottom, rows);
+                int cur_sum = getRec(lf, top, rows, bottom);
+                int this_row = getRec(rows, top, rows, bottom);
+
                 if(cur_sum<this_row){
                     lf = rows;
                     if(top != 0) down[top-1] = max(down[top-1], this_row);
@@ -85,14 +103,13 @@ int main(){
         right[i] = max(right[i], right[i + 1]);
     }
     int ans = 0;
+    int j = -1;
     for(int i =0; i<WIDTH; i++){
-
         ans = max(ans, up[i]+down[i]);
     }
     for(int i = 0; i<WIDTH; i++){
-
         ans = max(ans, left[i]+right[i]);
     }
-    cout<<ans+ans1;
+    cout << rn_amt + ans << endl;
 
 }
