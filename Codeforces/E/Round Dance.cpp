84a199f0b2450c00afaf39ca4fbb8f6f8b76502e
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-07-18 22:09:24 -0500

    Create Round Dance.cpp

---
+++ Codeforces/E/Round Dance.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i = 0; i<n; i++) cin>>arr[i];
+    vector<vector<int>> grid(n+1);
+    vector<int> dsu(n+1);
+    iota(dsu.begin(), dsu.end(), 0);
+    if(n == 2) cout<<"1 1\n";
+    else{
+        //all disconnected can be grouped into 1
+        // non disconnected must form own
+        //all other ones can form own for their own sake
+        for(int i =0; i<n; i++){
+
+        }
+    }
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
