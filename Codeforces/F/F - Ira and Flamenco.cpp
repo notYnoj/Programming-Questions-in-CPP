d861f544ef76793d4019bba1009e043d71f714c1
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-07-20 18:46:06 -0500

    Update F - Ira and Flamenco.cpp

--- Codeforces/F/F - Ira and Flamenco.cpp
+++ Codeforces/F/F - Ira and Flamenco.cpp
@@ -1,29 +1,72 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
 const int MOD = 1e9+7;
-long long choose(int n, int r){
-    long long sum = 1;
-    for(int i = 1; i <= r; i++){
-        sum = sum * (n - r + i) / i;
+ll modInverse(ll a, ll m) {
+    ll m0 = m;
+    ll y = 0, x = 1;
+    if (m == 1)
+        return 0;
+    while (a > 1) {
+        ll q = a / m;
+        ll t = m;
+        m = a % m, a = t;
+        t = y;
+        y = x - q * y;
+        x = t;
     }
-    return sum;
+    if (x < 0)
+        x += m0;
+    return x;
 }
-void solve(){
-    long long ans = 0;
+void solve() {
     int n, m;
-    cin>>n>>m;
+    cin >> n >> m;
     vector<int> a(n);
     map<int, int> mp;
-    for(int i=0; i<n; i++){
-        cin>>a[i];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
         mp[a[i]]++;
     }
     sort(a.begin(), a.end());
     auto it = unique(a.begin(), a.end());
     a.erase(it, a.end());
-    
-
-    cout<<ans<<"\n";
+    int l = 0;
+    int r = m - 1;
+    ll ans = 0;
+    ll cur = 1;
+    if (r >= a.size()) {
+        cout << "0\n";
+        return;
+    }
+    int nt = (int)a.size();
+    while(a[l]+m<=a[r] && r<nt){
+        l++;
+        r++;
+    }
+    for (int i = l; i <= r; i++) {
+        cur = (cur * mp[a[i]]) % MOD;  // Modified this line
+    }
+    ans = (ans + cur) % MOD;
+    while (r < nt) {
+        r++;
+        if(r == nt){
+            break;
+        }
+        cur = (cur * mp[a[r]]) % MOD;  
+        while(a[l]+m<=a[r] && l<r) {
+            cur = (cur * modInverse(mp[a[l]], MOD)) % MOD;  
+            l++;
+        }
+        if(r-l+1<m){
+            continue;
+        }
+        if(r-l+1>m){
+            cur = (cur * m) % MOD;  
+        }
+        ans = (ans + cur) % MOD;
+    }
+    cout << ans << '\n';
 }
 int main(){
     ios_base::sync_with_stdio(false);
