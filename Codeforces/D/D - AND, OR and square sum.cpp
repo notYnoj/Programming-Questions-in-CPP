a7d7de68136c01f655601e3dd74a2d96fe99535c
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-07-24 19:26:31 -0500

    Update D - AND, OR and square sum.cpp

--- Codeforces/D/D - AND, OR and square sum.cpp
+++ Codeforces/D/D - AND, OR and square sum.cpp
@@ -1,25 +1,46 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-int log(long long x) {
-    return 64 - __builtin_clzll(x) - 1;
-}
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
     int n;
     cin>>n;
-    vector<long long> arr(n);
-    vector<vector<long long>> hold(20);
-    for(int i = 0; i<n; i++) {
-        cin >> arr[i];
-        if(arr[i] != 0){
-            int z= log(arr[i]);
-            hold[z].push_back(arr[i]);
+    vector<ll> hold(n);
+    for(int i = 0; i<n; i++) cin>>hold[i];
+    vector<vector<int>> digits(n, vector<int>(20));
+    sort(hold.begin(), hold.end());
+    vector<vector<int>> mp(20);
+    for(int i = n-1; i>=0; i--){
+        for(int digit = 0; digit<20; digit++){
+            if(hold[i]>>digit & 1LL){
+                digits[i][digit]++;
+                mp[digit].push_back(i);
+            }
         }
     }
-
-    long long ans = 0;
-    if(n == 1){
-        cout<<arr[0]*arr[0];
-        return 0;
+    for(int i = n-1; i>=0; i--){
+        for(int bit = 19; bit>=0; bit--){
+            if(digits[i][bit] == 0 && !mp[bit].empty()){
+                int cur = mp[bit].back();
+                mp[bit].pop_back();
+                if(i<cur){
+                    continue;
+                }
+                digits[cur][bit] = 0;
+                digits[i][bit] = 1;
+            }
+        }
     }
-    for(int i = 0; i<n; i++) ans+= ((long long)arr[i]*arr[i]);
+    vector<ll> ans(n);
+    for(int i =0; i<n; i++){
+        for(int digit = 0; digit<20; digit++){
+            ans[i] += digits[i][digit]<<digit;
+        }
+    }
+    ll a = 0;
+    for(ll i: ans){
+        a+=(i*i);
+    }
+    cout<<a;
 }
