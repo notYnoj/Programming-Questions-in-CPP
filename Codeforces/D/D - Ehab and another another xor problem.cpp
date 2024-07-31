8411c6b9c8b8830ccad74dfa0432cbd055b40391
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-07-30 21:28:26 -0500

    Update D - Ehab and another another xor problem.cpp

--- Codeforces/D/D - Ehab and another another xor problem.cpp
+++ Codeforces/D/D - Ehab and another another xor problem.cpp
@@ -1,31 +1,29 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-const int MAX_BITS = 30;
+const int MAX_BITS = 29;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
     auto query = [&](ll a, ll b, int t) -> ll{
         if(t == 1){
             swap(a,b);
         }
 
-        cout<<"? "<<a<<' '<<b;
+        cout<<"? "<<a<<' '<<b<<endl;
         ll x;
         cin>>x;
-        if(x == -2){
-            exit(0);
-        }
         if(t == 1 && x!=0){
             (x==1?x = -1:x=1);
         }
-        fflush(stdout);
 
         return x;
     };
-    int t = query(0, 0, -2);
+    ll t = query(0, 0, -2);
     if(t == 0){
         vector<ll> number(30, 0);
         ll new1 = 0;
-        for(int i = 0; i<MAX_BITS; i++){
+        for(ll i = 0; i<=MAX_BITS; i++){
             ll z = query((new1+(1LL<<i)), new1, 0);
             if(z == -1){
                 number[i] = 1;
@@ -35,7 +33,7 @@ int main(){
             }
         }
         ll ans = 0;
-        for(int i = 0; i<MAX_BITS; i++){
+        for(ll i = 0; i<=MAX_BITS; i++){
             ans+=number[i]<<i;
         }
         cout<<"! "<<ans<<' '<<ans;
@@ -45,7 +43,7 @@ int main(){
         vector<ll> smaller(30);
         vector<ll> bigger(30);
         bool change = false;
-        for(int bit = MAX_BITS; bit>=0; bit--){
+        for(ll bit = MAX_BITS; bit>=0; bit--){
             ll cur_sum = 0;
             ll cur_bigger_sum = 0;
             for(ll i = 29; i>bit; i--){
@@ -55,12 +53,19 @@ int main(){
             //changing it if bigger changes
             (!change?cur_sum+=(1<<bit):cur_bigger_sum+=(1<<bit));
             ll cur = query(cur_sum, cur_bigger_sum, t);
-
+            if(change){
+                (cur==1?cur=-1:cur=1);
+            }
             if(cur == 0) {
                 (!change? smaller[bit] = 0:smaller[bit] =1 );
                 (!change? bigger[bit] = 1:bigger[bit] = 0);
-                (!change?cur_bigger_sum+=1LL<<bit:cur_sum+= 1LL<<bit);
-                for(int i = bit-1; i>=0; i--){
+                cur_sum = 0;
+                cur_bigger_sum = 0;
+                for(ll i = MAX_BITS; i>=bit; i--){
+                    cur_sum+=(smaller[i]<<i);
+                    cur_bigger_sum+=(bigger[i]<<i);
+                }
+                for(ll i = bit-1; i>=0; i--){
                     ll z = query(cur_sum+(1LL<<i), cur_bigger_sum,t);
                     if(z == -1){
                         bigger[i] = smaller[i] =  1;
@@ -74,7 +79,9 @@ int main(){
             }
             (!change?cur_bigger_sum+=(1<<bit):cur_sum+=(1<<bit));
             ll new_cur = query(cur_sum, cur_bigger_sum, t);
-            assert(new_cur != 0);
+            if(change){
+                (new_cur==1?new_cur=-1:new_cur=1);
+            }
             //calm luh notebook
             if(cur == 1){
                 if(new_cur == 1){
@@ -98,14 +105,13 @@ int main(){
 
         ll a = 0;
         ll b = 0;
-        for(int i = 0; i<MAX_BITS; i++){
+        for(ll i = 0; i<=MAX_BITS; i++){
             a+=smaller[i]<<i;
             b+=bigger[i]<<i;
         }
         if (t == 1) {
             swap(a, b);
         }
-        cout<<"! "<<a<<' '<<b;
-        fflush(stdout);
+        cout<<"! "<<a<<' '<<b<<endl;
     }
 }
