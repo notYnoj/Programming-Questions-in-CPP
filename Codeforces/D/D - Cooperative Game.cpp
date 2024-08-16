34ee7a29f6e950697a887a9bed20569bec2c9372
Ynoj <117422590+notYnoj@users.noreply.github.com>
2024-08-15 19:06:28 -0500

    Create D - Cooperative Game.cpp

---
+++ Codeforces/D/D - Cooperative Game.cpp
@@ -0,0 +1,14 @@
+/*
+Subproblem 1- Finding "entrance" in a cycle
+We can simply find distance fast is from entrance when slow enters
+cycle fast decreases distance by 1, thus fast's distance from entrance is
+o<-o distance is shown as such (theres paper image that looks better i swear)
+will meet distance away.
+
+Subproblem 2 - finding t and c
+Find C - Move 1 until meets, this is C (unnecssary?)
+Find T - remember slow moved x units, thus x - distance = t
+
+Move all pieces
+
+*/
