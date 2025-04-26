414ad42a6d61da9798784f3717c59bfaad329243
Ynoj <117422590+notYnoj@users.noreply.github.com>
2025-04-26 00:13:35 -0500

    Update B.cpp

--- Codeforces/Contests/Div 3/1020/B.cpp
+++ Codeforces/Contests/Div 3/1020/B.cpp
@@ -1,1 +1,41 @@
 
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+using namespace std;
+
+// Everything Else thats new :D
+template <typename T> //custom output stream operator for vector
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
+    for (const auto& elem : vec) {
+        os << elem << " ";
+    }
+    return os;
+}
+void solve(){
+    int n,x;
+    cin>>n>>x;
+    vector<int> ans(n);
+    int ptr = 0;
+    for(int i = 0; i<x; i++){
+        ans[ptr++] = i;
+    }
+    for(int i = n-1; i>=x; i--){
+        ans[ptr++] = i;
+    }
+    cout<<ans<<endl;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
