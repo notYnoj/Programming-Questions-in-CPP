47cebe634ae68fcd1bbe3b0506642ebbbe689b53
Ynoj <117422590+notYnoj@users.noreply.github.com>
2026-08-04 12:12:07 -0500

    Update A.cpp

--- Codeforces/Contests/1114/A.cpp
+++ Codeforces/Contests/1114/A.cpp
@@ -1,1 +1,71 @@
 
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<ll>
+#define vvi vector<vector<ll>>
+#define nl '\n'
+#include <chrono>
+using namespace std;
+int t;
+// Everything Else thats new :D
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::pair<T, T> p){
+    os<<p.first<<' '<<p.second<<endl;
+    return os;
+}
+template <typename T> //custom output stream operator for vector
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
+    for (const auto& elem : vec) {
+        os << elem << ' ';
+    }
+    return os;
+}
+
+
+template <typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& vec){
+    //of size n
+    for(T& elem: vec){
+        is>>elem;
+    }
+    return is;
+}
+
+
+void solve(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    vi n;
+    n.pb(a);
+    n.pb(b);
+    n.pb(c);
+    sort(all(n));
+    for(int i = 0; i<1; i++){
+        if(n[i] == n[i+1]){
+            cout<<0<<nl;
+            return;
+        }
+    }
+    //1 2 3
+    cout<<min(n[2]-n[1], (n[2]-n[0])/2)<<nl;
+}
+int main(){
+    #ifdef DEBUG
+    auto start = std::chrono::high_resolution_clock::now();
+    #endif
+    
+    cin>>t;
+    while(t--){solve();}
+    
+    #ifdef DEBUG
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    cout << "\n-----------------------------" << endl;
+    cout << "Time taken: " << duration.count() << " milliseconds" << endl;
+    return 0;
+    #endif
+}
